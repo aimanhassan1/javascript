f24f9ae6049f874f0fd1ff7543aa5a11782d7d0d
aimanhassan1 <aimanhassan180@gmail.com>
2025-09-24 11:44:56 +0500

    Add files via upload

---
+++ aimanjavascript/index.js
@@ -0,0 +1,26 @@
+let x=23
+let y=4
+document.write(x + y + "<br>")
+const item=10
+document.write(item + "<br>")
+// var
+var box="welcome"
+document.write(box +"<br>")
+let num1=30
+let num2=80
+let num3 = (num1>num2)
+document.write(num3)
+if(20<=20 && 50<=50){
+  document.write(true +"<br>")
+}
+else{                                   
+  document.write(false +"<br>")
+}
+if(20<=30||10<=10){
+  document.write(true +"<br>")
+}
+else{                                   
+  document.write(false +"<br>")
+}
+let result=((50<=20)? 'pass':'fail')
+document.write(result)
